6fbdb6917fca84719eb06e322ac9e6e11f2f6672
student <student@ip-172-31-52-69.us-west-2.compute.internal>
2021-07-22 16:55:26 -0700

    TeaMaker

--- src/_02_smurf/SmurfsRunner.java
+++ src/_02_smurf/SmurfsRunner.java
@@ -8,10 +8,14 @@ public static void main(String[] args) {
 	Smurf Papa = new Smurf("Papa");
 	Papa.getName();
 	Papa.getHatColor();
+	System.out.println("Papa smurfs' hat is "+Papa.getHatColor() );
 	Papa.isGirlOrBoy();
+	System.out.println("Papa smurf is a "+Papa.isGirlOrBoy());
 	Smurf Smurfette = new Smurf("Smurfette");
 	Smurfette.getName();
 	Smurfette.getHatColor();
+	System.out.println("Smurfette's hat is "+Smurfette.getHatColor());
 	Smurfette.isGirlOrBoy();
+	System.out.println("Smurfette is a "+Smurfette.isGirlOrBoy());
 }
 }
---
+++ src/_03_tea_maker/TeaMakerRunner.java
@@ -0,0 +1,10 @@
+package _03_tea_maker;
+
+public class TeaMakerRunner {
+	public static void main(String[] args) {
+		
+	
+Kettle Bob = new Kettle();
+	Bob.getWater();
+	}
+}
