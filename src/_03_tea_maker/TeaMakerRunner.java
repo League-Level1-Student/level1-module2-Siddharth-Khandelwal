b26c4b0db12bc58fd79294dd2ccfa3ad75171522
student <student@ip-172-31-53-49.us-west-2.compute.internal>
2021-07-29 16:51:43 -0700

    TeaMaker

--- src/_03_tea_maker/TeaMakerRunner.java
+++ src/_03_tea_maker/TeaMakerRunner.java
@@ -1,13 +1,17 @@
 package _03_tea_maker;
 
+import _03_tea_maker.Kettle.Water;
+
 public class TeaMakerRunner {
 	public static void main(String[] args) {
 		
 	
-Kettle Bob = new Kettle("Bob");
-	Bob.getWater();
+Kettle Bob = new Kettle();
+TeaBag Jeff = new TeaBag("Mint");
+Cup Matt = new Cup();
+
 	Bob.boil();
-	
+	Matt.makeTea(Jeff, Bob.getWater());
 	}
 	
 }
